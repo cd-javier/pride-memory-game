2f4e85db18378c437432403b117c2bbd40b7af68
Javier Quiroga <contact@javierquiroga.com>
2025-07-13 19:51:11 +0100

    feat: start Game component - Create preapreDeck helper function with Fisher-Yates shuffle method - Prepare props and functions that will be dev'd later - Write selectCard function

--- src/Game/Game.jsx
+++ src/Game/Game.jsx
@@ -1,1 +1,51 @@
+import { useState } from 'react';
+import { useRef } from 'react';
 import './Game.css';
+import Card from './Card';
+import shuffle from '../utils/shuffle';
+
+function prepareDeck(deck) {
+  const doubledDeck = [...deck, ...deck].map((card, index) => {
+    return {
+      ...card,
+      id: index,
+      isSelected: false,
+      isDiscovered: false,
+    };
+  });
+
+  return shuffle(doubledDeck);
+}
+
+export default function Game({ originalDeck, triggerDiscover }) {
+  const [deck, setDeck] = useState(() => prepareDeck(originalDeck));
+  const prevCard = useRef([]);
+
+  function selectCard(card) {
+    const newDeck = [...deck].map((elm) => {
+      if (elm.id === card.id) {
+        return { ...elm, isSelected: true };
+      } else {
+        return elm;
+      }
+    });
+
+    setDeck(newDeck);
+  }
+
+  function deselectCards() {}
+
+  function discoverCard(card) {}
+
+  function handleFlip(card) {
+    selectCard(card);
+  }
+
+  return (
+    <div className="game">
+      {deck.map((card) => (
+        <Card card={card} key={card.id} handleFlip={handleFlip} />
+      ))}
+    </div>
+  );
+}
