1bb969d8db0f7fcc00f8a8a7d69868fb7d9a0d2c
Javier Quiroga <contact@javierquiroga.com>
2025-07-13 19:15:10 +0100

    feat: create Card component

--- src/Game/Card.jsx
+++ src/Game/Card.jsx
@@ -1,1 +1,23 @@
 import './Card.css';
+
+export default function Card({ card, handleFlip }) {
+  function handleClick() {
+    if (!card.isSelected && !card.isDiscovered) {
+      handleFlip(card);
+    }
+  }
+  return (
+    <div className="cardContainer" onClick={handleClick}>
+      <div
+        className={
+          card.isSelected || card.isDiscovered ? 'card is-flipped' : 'card'
+        }
+      >
+        <div className="card-face card-front"></div>
+        <div className="card-face card-back">
+          <img src={card.img} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+}
