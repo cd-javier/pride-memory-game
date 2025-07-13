f6358b9e8da629f13a03b73efd552c6794ae0f82
Javier Quiroga <contact@javierquiroga.com>
2025-07-13 19:14:57 +0100

    feat: prepare dummy deck for dev

--- src/deck.js
+++ src/deck.js
@@ -1,1 +1,23 @@
-export const deck = [{}];
+import progressFlag from './assets/flags/progress-pride.jpg';
+import originalFlag from './assets/flags/original-pride.gif';
+import transFlag from './assets/flags/trans-pride.png';
+
+const deck = [
+  {
+    keyword: 'original',
+    img: originalFlag,
+    description: 'Created by Gilbert Baker in 1978.',
+  },
+  {
+    keyword: 'progress',
+    img: progressFlag,
+    description: 'Created by Daniel Quasar in 2018.',
+  },
+  {
+    keyword: 'trans',
+    img: transFlag,
+    description: 'Created by Monica Helms in 1999.',
+  },
+];
+
+export default deck;
