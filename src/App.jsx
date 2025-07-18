8cf735d45fb4d343b99121fe60c7108f7c1269ea
Javier Quiroga <contact@javierquiroga.com>
2025-07-18 16:31:02 +0100

    feat: Enable toggling in legend cards

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,28 @@ function App({ deck }) {
   const [discoveredCards, setDiscoveredCards] = useState([]);
 
   function triggerDiscover(card) {
-    setDiscoveredCards([card, ...discoveredCards]);
+    setDiscoveredCards([{ ...card, isOpen: false }, ...discoveredCards]);
+  }
+
+  function handleLegendToggle(card) {
+    setDiscoveredCards(
+      [...discoveredCards].map((elm) => {
+        if (elm === card) {
+          return { ...card, isOpen: !elm.isOpen };
+        } else return { ...card, isOpen: false };
+      })
+    );
   }
 
   return (
-    <>
+    <div className="app">
       <Game originalDeck={deck} triggerDiscover={triggerDiscover} />
-      <Legend deck={discoveredCards} totalPairs={deck.length} />
-    </>
+      <Legend
+        deck={discoveredCards}
+        totalPairs={deck.length}
+        handleLegendToggle={handleLegendToggle}
+      />
+    </div>
   );
 }
 
--- src/Legend/Legend.jsx
+++ src/Legend/Legend.jsx
@@ -1,25 +1,52 @@
-function SingleLegend({ card }) {
+import './Legend.css';
+
+function SingleLegend({ card, handleToggle }) {
+  function handleClick() {
+    handleToggle(card);
+  }
+
   return (
     <div className="single-legend">
       <img src={card.img} alt={card.heading} />
       <div className="heading">{card.heading}</div>
-      <div className="description">{card.description}</div>
+      {card.isOpen && <div className="description">{card.description}</div>}
+      <div className="arrow" onClick={handleClick}>
+        {card.isOpen ? (
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <title>menu-up</title>
+            <path d="M7,15L12,10L17,15H7Z" />
+          </svg>
+        ) : (
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <title>menu-down</title>
+            <path d="M7,10L12,15L17,10H7Z" />
+          </svg>
+        )}
+      </div>
     </div>
   );
 }
 
-export default function Legend({ deck, totalPairs }) {
+export default function Legend({ deck, totalPairs, handleLegendToggle }) {
+  function handleToggle(card) {
+    handleLegendToggle(card);
+  }
+
   return (
     <div className="legend">
-      {deck.length === 0 ? (
-        <div> Empty state </div>
-      ) : (
-        deck.map((card, index) => <SingleLegend card={card} key={index} />)
-      )}
       <p>
         {deck.length} {deck.length === 1 ? 'pair' : 'pairs'} discovered out of{' '}
         {totalPairs}
       </p>
+      {deck.length === 0 ? (
+        <div className="empty-state">
+          <p>Find a match to start learning</p>
+        </div>
+      ) : (
+        deck.map((card, index) => (
+          <SingleLegend card={card} key={index} handleToggle={handleToggle} />
+        ))
+      )}
     </div>
   );
 }
