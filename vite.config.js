30f78d903b53bfbc3600d0fcd760edb4f90c0cc5
Javier Quiroga <contact@javierquiroga.com>
2025-07-21 16:02:28 +0100

    fix: Fix issues with deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: '/base-url',
+  base: '/pride-memory-game',
   plugins: [react()],
 });
